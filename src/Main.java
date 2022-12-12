cfdba8f99c41f11c10b0764d72dc3e0745da2ff4
Yusuf Sönmez <yusufsonmez951@gmail.com>
2022-12-12 23:08:17 +0300

    recep halis mulis dünyanın en iyi adamıdır

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,6 @@ public class Main {
         accounts.get(0).setId(0);
         new MainMenu().mainMenu();
         System.out.println("Enes abim.");
+        System.out.println("Orkun recep");
     }
 }
