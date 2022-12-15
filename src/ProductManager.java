5358c8b61752fc48b638769f95cbeefecdc6d725
orkunyalcinkaya <orkunnuriyalcinkaya@gmail.com>
2022-12-15 18:22:50 +0300

    add product komutu eklendi.

--- src/ProductManager.java
+++ src/ProductManager.java
@@ -1,23 +1,32 @@
 public class ProductManager extends AdminMenu {
     public void addProduct() {
-        int categoryId;
-        int price;
-        String companyName;
-        String productName;
-        System.out.println("Sırasıyla eklenecek ürünün Kategori id'sini, ücretini, markasını ve ürün adını giriniz ->");
-        categoryId = scanner.nextInt();
-        price = scanner.nextInt();
-        companyName = scanner.next();
-        productName = scanner.next();
-        if (categoryId == 1) {
+        boolean loop = true;
+        while (loop) {
+            int categoryId;
+            double price;
+            String companyName;
+            String productName;
+            System.out.println("Sırasıyla eklenecek ürünün Kategori id'sini, ücretini, markasını ve ürün adını giriniz ->");
+            categoryId = scanner.nextInt();
+            price = scanner.nextInt();
+            companyName = scanner.next();
+            productName = scanner.next();
+            if (categoryId == 1) {
+                products.add(new ElectronicProduct(price, companyName, productName));
+                loop = false;
+            } else if (categoryId == 2) {
+                products.add(new FurnitureProduct(price, companyName, productName));
+                loop = false;
+            } else if (categoryId == 3) {
+                products.add(new HouseholdAppliancesProduct(price, companyName, productName));
+                loop = false;
+            } else {
+                System.out.println("Yanlış kategori id'si girildi lütfen tekrar deneyiniz");
+            }
 
-        } else if (categoryId == 2) {
 
-        } else if (categoryId == 3) {
-
-        } else {
-            System.out.println("Yanlış kategori id'si girildi lütfen tekrar deneyiniz");
         }
+
     }
 
     public void removeProduct() {
@@ -25,10 +34,10 @@ public class ProductManager extends AdminMenu {
     }
 
     public void list() {
-        for (BaseProduct product : products){
+        for (BaseProduct product : products) {
             System.out.println("\n\nKategori Id : " + product.getCatagoryId());
             System.out.println("Ürün Id : " + product.getProductId());
-            System.out.println("Ürün adı : " + product.productName);
+            System.out.println("Ürün Adı : " + product.productName);
             System.out.println("Ürün Fiyatı : " + product.price);
 
 
